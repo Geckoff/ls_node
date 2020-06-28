5a6704c2f042b733123df1c3d808c4011dd304eb
Geckoff <ag-geck@yandex.ru>
2020-06-27 18:35:17 -0600

    Implemented websocket

--- hw4/backend/app.js
+++ hw4/backend/app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./router/index");
+const initIo = require("./websocket");
+const http = require("http");
+const socketIo = require("socket.io");
+
 const cors = require("cors");
 require("./db");
 
 var app = express();
+const server = http.createServer(app);
+const io = socketIo.listen(server);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/", router);
 
-const server = app.listen(3000, function () {
+server.listen(3000, function () {
 	console.log("Go to http://localhost:" + server.address().port);
 });
+
+initIo(io);
